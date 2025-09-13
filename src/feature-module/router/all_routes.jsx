047fbfa55d432f936cfff92b2adb02e3ca31689a
Mona Lisa <sohamsolat@gmail.com>
2025-09-13 20:50:20 +0530

    student,admin,parent Dashboard

--- src/feature-module/router/all_routes.jsx
+++ src/feature-module/router/all_routes.jsx
@@ -1,10 +1,29 @@
 import TeacherDashboard from "../mainMenu/teacherDashboard";
+import AdminDashboard from "../mainMenu/adminDashboard";
+//import parentDashboard from "../mainMenu/parentDashboard";
+
 
 export const all_routes = {
+
+  // dashboard routes
+    
+
   login: "/login",
   forgotPassword: "/forgot-password",
   register:"/register",
   resetPassword: "/reset-password",
   resetPasswordSuccess: "/reset-password-success",
+
+adminDashboard: "/index",
   teacherDashboard: "/teacher-dashboard",
+    parentDashboard: "/parent-dashboard",
+      studentDashboard: "/student-dashboard",
+
+
+
+
+  //  parentGrid: "/parent/parent-grid",
+  // parentList: "/parent/parent-list",
+  // guardiansGrid: "/parent/guardians-grid",
+  // guardiansList: "/parent/guardians-list",
 };
--- src/feature-module/router/router.link.jsx
+++ src/feature-module/router/router.link.jsx
@@ -6,13 +6,63 @@ import Register from "@/feature-module/auth/register/Register";
 import ResetPassword from "@/feature-module/auth/resetPassword/resetPassword";
 import ResetPasswordSuccess from "@/feature-module/auth/resetPasswordSuccess/resetPasswordSuccess";
 import TeacherDashboard from "@/feature-module/mainMenu/teacherDashboard";
+import AdminDashboard from "@/feature-module/mainMenu/adminDashboard";
+import ParentDashboard from "@/feature-module/mainMenu/parentDashboard";
+import StudentDasboard from "@/feature-module//mainMenu/studentDashboard";
+
+//import ParentList from "../peoples/parent/parent-list";
+
+
+
 const routes = all_routes;
 export const publicRoutes = [
   {
     path: routes.teacherDashboard,
     element: <TeacherDashboard />,
     route: Route,
+  }, 
+  {
+    path: routes.parentDashboard,
+    element: <ParentDashboard />,
+    route: Route,
   },
+   {
+    path: routes.adminDashboard,
+    element: <AdminDashboard />,
+    route: Route,
+  },
+  {
+    path: routes.studentDashboard,
+    element: <StudentDasboard />,
+    route: Route,
+  },
+
+
+
+// {
+//     path: routes.parentGrid,
+//     element: <ParentGrid />,
+//   },
+//   {
+//     path: routes.parentList,
+//     element: <ParentList />,
+//   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ];
 export const authRoutes = [
   {
